96a1901d8d4eb5a180cae72d69fd88e27d1ef045
Ajeetkumar Biradar <136881345+ajeetkumar-biradar@users.noreply.github.com>
2024-05-20 21:07:19 +0530

    Add files via upload

--- flask_todo/app.py
+++ flask_todo/app.py
@@ -3,18 +3,14 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Initialize a list to store tasks
 tasks = []
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get the task from the form
         task = request.form['task']
-        # Add the task to the list
         tasks.append(task)
-        # Redirect to the home page
         return redirect(url_for('index'))
     return render_template('index.html', tasks=tasks)
 
