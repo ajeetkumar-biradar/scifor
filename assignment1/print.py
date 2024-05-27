ab0138d1fff7a6dde638c35b0635cd4ffd12dac5
Ajeetkumar Biradar <136881345+ajeetkumar-biradar@users.noreply.github.com>
2024-05-27 11:51:56 +0530

    Delete assignment1 directory

--- assignment1/print.py
+++
@@ -1,3 +0,0 @@
-# Arvind just started learning python and he is very interested to know how he can print his name on the screen using python. Write a program to print Arvind on the screen. Print your name and age as well using a python print statement.
-
-print("Aravind")
